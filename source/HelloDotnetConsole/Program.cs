d1aec787e9d08f8b3ccf74b7ade368c7e8b53ba5
eriawan <eriawan@live.com>
2024-08-15 07:05:01 +0700

    Commit all from week 01 to week 05

--- source/HelloDotnetConsole/Program.cs
+++ source/HelloDotnetConsole/Program.cs
@@ -4,3 +4,4 @@ using System.Runtime.InteropServices;
 Console.WriteLine($"Hello, .NET version: {Environment.Version.ToString()}!");
 var currentDate = DateTime.Now;
 Console.WriteLine($"Current date and time is: {currentDate.ToString("MM/dd/yyyy HH:mm:ss")}");
+Console.ReadLine();
---
+++ source/SampleWinformsNet80/Form1.cs
@@ -0,0 +1,11 @@
+namespace SampleWinformsNet80;
+
+public partial class Form1 : Form
+{
+    public Form1()
+    {
+        InitializeComponent();
+
+        Button button = new Button();
+    }
+}
