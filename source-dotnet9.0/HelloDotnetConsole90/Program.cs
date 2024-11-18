c2133a3c8350c5ed04ffa275cea780ab9962af79
eriawan <eriawan@live.com>
2024-11-19 00:52:29 +0700

    add what's new in .NET 9.0 sample codes

---
+++ source-dotnet9.0/HelloDotnetConsole90/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.InteropServices;
+
+Console.WriteLine($"Hello, .NET version: {Environment.Version.ToString()}!");
+var currentDate = DateTime.Now;
+Console.WriteLine($"Current date and time is: {currentDate.ToString("MM/dd/yyyy HH:mm:ss")}");
+Console.ReadLine();
