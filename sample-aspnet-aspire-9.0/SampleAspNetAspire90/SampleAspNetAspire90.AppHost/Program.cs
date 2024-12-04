40f90d5df1d3e1f271528ce9de1e10678f56724f
eriawan <eriawan@live.com>
2024-12-05 04:22:33 +0700

    Update sample starter app to have ability to set redis insgiht and redis commander to enable container lifetime persistent. Also update .NET SDK setting in global.json to use updated 9.0.101 SDK to ensure that it follows default behavior of NuGetAuditMode set to direct. See also https://developercommunity.visualstudio.com/t/NuGet-errors-after-upgrading-from-VS-v17/10791809 And official docs of NugetAuditMode: https://learn.microsoft.com/en-us/dotnet/core/compatibility/sdk/9.0/nugetaudit-transitive-packages#reason-for-change

--- sample-aspnet-aspire-9.0/SampleAspNetAspire90/SampleAspNetAspire90.AppHost/Program.cs
+++ sample-aspnet-aspire-9.0/SampleAspNetAspire90/SampleAspNetAspire90.AppHost/Program.cs
@@ -4,8 +4,20 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var cache = builder.AddRedis("cache")
     .WithLifetime(ContainerLifetime.Persistent)
-    .WithRedisInsight()
-    .WithRedisCommander();
+    .WithRedisInsight((redisInsight) => {
+        redisInsight.WithLifetime(ContainerLifetime.Persistent);
+    })
+    .WithRedisCommander((rediscommander) => {
+        rediscommander.WithLifetime(ContainerLifetime.Persistent);
+    });
+
+//var cache = builder.AddRedis("cache")
+//    .WithLifetime(ContainerLifetime.Persistent)
+//    .WithRedisInsight()
+//    .WithRedisCommander();
+
+var sqlserverdb = builder.AddSqlServer("sqlserverdb", port: 1500)
+    .WithLifetime(ContainerLifetime.Persistent);
 
 var apiService = builder.AddProject<Projects.SampleAspNetAspire90_ApiService>("apiservice")
     .WithReference(cache)
